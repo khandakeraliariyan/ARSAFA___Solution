af9881bbb846a0b056dca3519552b184e8387bf3
Fahim Siddiqui <87389989+fahim0710@users.noreply.github.com>
2025-12-07 20:01:55 +0600

    urls.py

--- ARSAFA___SOLUTION/urls.py
+++ ARSAFA___SOLUTION/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', accounts_views.admin_dashboard, name='admin_dashboard'),
     path('', include('accounts.urls')),
     path('inventory/', include('inventory.urls')),
     path('sales/', include('sales.urls')),
