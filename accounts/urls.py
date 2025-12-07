519570957ec09fd5028f74e502de7fa438fe6ebc
Fahim Siddiqui <87389989+fahim0710@users.noreply.github.com>
2025-12-07 20:20:36 +0600

    urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from .views import custom_login, admin_dashboard, custom_login_redirect, logout_view
 
 urlpatterns = [
     path('dashboard/', admin_dashboard, name='admin_dashboard'),
